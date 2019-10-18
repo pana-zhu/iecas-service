c8a2b88914eb50cbf7c55d582cfa84e7011f1326
pana-zhu <502285157@qq.com>
2019-10-18 11:39:02 +0800

    Initial commit

---
+++ course-dubbo-service/src/main/java/com/imooc/course/CourseServiceApplication.java
@@ -0,0 +1,14 @@
+package com.imooc.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Michael on 2017/10/28.
+ */
+@SpringBootApplication
+public class CourseServiceApplication {
+    public static void main(String args[]) {
+        SpringApplication.run(CourseServiceApplication.class, args);
+    }
+}
---
+++ user-thrift-service/src/main/java/com/imooc/user/thrift/ThriftServer.java
@@ -0,0 +1,57 @@
+package com.imooc.user.thrift;
+
+import com.imooc.thrift.user.UserService;
+import org.apache.thrift.TProcessor;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.server.TNonblockingServer;
+import org.apache.thrift.server.TServer;
+import org.apache.thrift.transport.TFramedTransport;
+import org.apache.thrift.transport.TNonblockingServerSocket;
+import org.apache.thrift.transport.TTransportException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * Created by Michael on 2017/10/28.
+ */
+@Configuration
+public class ThriftServer {
+
+	@Value("${service.port}")
+	private int servicePort;
+
+	@Autowired
+	private UserService.Iface userService;
+
+	/*
+	 * 启动thrift server
+	 */
+	@PostConstruct
+	public void startThriftServer() {
+
+		TProcessor processor = new UserService.Processor<>(userService);
+
+		/**
+		 * IO 模型 ，单线程 线程池 NIO 
+		 * TNonblockingServerSocket NIO 模型
+		 */
+		TNonblockingServerSocket socket = null;
+		try {
+			socket = new TNonblockingServerSocket(servicePort,60000);
+		} catch (TTransportException e) {
+			e.printStackTrace();
+		}
+		TNonblockingServer.Args args = new TNonblockingServer.Args(socket);
+		args.processor(processor);
+		// 传输方式 （帧传输）
+		args.transportFactory(new TFramedTransport.Factory());
+		// 传输协议 （二进制） json protocol
+		args.protocolFactory(new TBinaryProtocol.Factory());
+		TServer server = new TNonblockingServer(args);
+		
+		server.serve();
+	}
+}
